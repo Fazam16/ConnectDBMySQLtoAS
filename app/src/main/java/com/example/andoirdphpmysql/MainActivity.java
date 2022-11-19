86faa4534918c8a0be2b6a931fa7a20891f7c21d
Fazam16 <fadilzahran0123@gmail.com>
2022-11-19 09:48:18 +0700

    Menambahkan file SharedPrefManager dan RequestHandler

--- app/src/main/java/com/example/andoirdphpmysql/MainActivity.java
+++ app/src/main/java/com/example/andoirdphpmysql/MainActivity.java
@@ -2,21 +2,20 @@ package com.example.andoirdphpmysql;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editTextUsername, editTextPassword, editTextEmail;
     private Button buttonRegister;
     private ProgressDialog progressDialog;
+
+    private TextView textViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +36,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
 
+        textViewLogin = findViewById(R.id.textViewLogin);
+
         buttonRegister = findViewById(R.id.buttonRegister);
         progressDialog = new ProgressDialog(this);
 
         buttonRegister.setOnClickListener(this);
-
-
+        textViewLogin.setOnClickListener(this);
     }
 
     private void registerUser() {
@@ -73,13 +75,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return params;
             }
         };
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
     }
 
     @Override
     public void onClick(View view) {
         if(view == buttonRegister)
             registerUser();
+        if(view ==textViewLogin)
+            startActivity(new Intent(this, LoginActivity.class));
     }
 }
